96a24d18d9250906951edb6845240b01eb67fa54
kvnq-poza <kvnqpoza@gmail.com>
2024-12-16 19:16:29 +0100

    Added Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,54 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import { PlaylistProvider } from "./context/PlaylistContext";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import PlaylistConverter from "./components/PlaylistConverter";
 import FAQSection from "./components/FAQSection";
+import TermsAndPrivacy from "./components/TermsAndPrivacy";
 
 function App() {
   return (
-    <PlaylistProvider>
-      <div className="flex flex-col min-h-screen bg-gray-100">
-        <Header />
-        <main className="flex-grow">
-          <PlaylistConverter />
-          <FAQSection />
-        </main>
-        <Footer />
-        <Toaster
-          position="top-right"
-          toastOptions={{
-            success: {
-              style: {
-                background: "#4CAF50",
-                color: "white",
+    <Router>
+      <PlaylistProvider>
+        <div className="flex flex-col min-h-screen bg-gray-100">
+          <Header />
+          <main className="flex-grow">
+            <Routes>
+              <Route
+                path="/"
+                element={
+                  <>
+                    <PlaylistConverter />
+                    <FAQSection />
+                  </>
+                }
+              />
+              <Route path="/terms" element={<TermsAndPrivacy />} />
+            </Routes>
+          </main>
+          <Footer />
+          <Toaster
+            position="top-right"
+            toastOptions={{
+              success: {
+                style: {
+                  background: "#4CAF50",
+                  color: "white",
+                },
               },
-            },
-            error: {
-              style: {
-                background: "#F44336",
-                color: "white",
+              error: {
+                style: {
+                  background: "#F44336",
+                  color: "white",
+                },
               },
-            },
-          }}
-        />
-      </div>
-    </PlaylistProvider>
+            }}
+          />
+        </div>
+      </PlaylistProvider>
+    </Router>
   );
 }
 
