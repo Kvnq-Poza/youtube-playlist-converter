26301c234bdb45ac48367fcc93f62fcbdb440ce7
kvnq-poza <kvnqpoza@gmail.com>
2024-12-16 19:17:02 +0100

    Update head to route home

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,27 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { FaYoutube, FaMusic } from "react-icons/fa";
 import { motion } from "framer-motion";
 
 const Header = () => {
   return (
     <header className="bg-red-600 text-white py-4 shadow-md px-5">
-      <div className="container mx-auto flex items-center justify-center">
-        <motion.div
-          className="flex items-center space-x-3"
-          initial={{ scale: 0.9 }}
-          animate={{ scale: 1 }}
-          transition={{ type: "spring", stiffness: 300 }}
+      <div className="container mx-auto">
+        <Link
+          to="/"
+          className="flex items-center justify-center hover:opacity-80 transition-opacity duration-300"
         >
-          <FaYoutube className="text-4xl" />
-          <h1 className="text-2xl font-bold">YouTube Playlist Converter</h1>
-          <FaMusic className="text-3xl" />
-        </motion.div>
+          <motion.div
+            className="flex items-center space-x-3"
+            initial={{ scale: 0.9 }}
+            animate={{ scale: 1 }}
+            transition={{ type: "spring", stiffness: 300 }}
+          >
+            <FaYoutube className="text-4xl" />
+            <h1 className="text-2xl font-bold">YouTube Playlist Converter</h1>
+            <FaMusic className="text-3xl" />
+          </motion.div>
+        </Link>
       </div>
     </header>
   );
